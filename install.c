e22932f29d4643cbf29ae93a57fe30dc17d65566
Garrett Credi <gcc@ameritech.net>
2016-07-15 19:01:26 -0500

    Create install.c

---
+++ install.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+printf("Installing hangman onto system\n");
+printf("Creating hangman binary\n");
+system("clang hangman.c -o hangman -lcurl");
+printf("Creating absolute file locaiton\n");
+system("cp ./hangman ~/");
+printf("Adding command to bash_profile\n");
+system("echo 'hangman(){ ~/hangman } echo \"use hangman to play hangman\"'>> ~/.bash_profile");
+printf("Finished!\n");
+printf("Input hangman into your console to play!\n");
+}

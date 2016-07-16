be6cf8e39129f2dc5ea82a7afc5e902659bffd25
Garrett Credi <gcc@ameritech.net>
2016-07-15 19:20:24 -0500

    Update install.c

--- install.c
+++ install.c
@@ -7,9 +7,7 @@ system("clang hangman.c -o hangman -lcurl");
 printf("Creating absolute file locaiton\n");
 system("cp ./hangman ~/");
 printf("Adding command to bash_profile\n");
-system("echo 'hangman(){ 
-  ~/hangman 
-}'>> ~/.bash_profile");
+system("echo 'hangman(){ ~/hangman }'>> ~/.bash_profile");
 printf("Finished!\n");
 printf("Now just close and re-open your console and then input hangman into your console to play!\n");
 }

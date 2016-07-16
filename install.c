cba978e768d2125886d7bf53c765ae7b9c990be5
Garrett Credi <gcc@ameritech.net>
2016-07-15 19:24:13 -0500

    Update install.c

--- install.c
+++ install.c
@@ -3,7 +3,7 @@
 int main(void){
 printf("Installing hangman onto system\n");
 printf("Creating hangman binary\n");
-system("clang hangman.c -o hangman -lcurl");
+system("clang hangmanv1.c -o hangman -lcurl");
 printf("Creating absolute file locaiton\n");
 system("cp ./hangman ~/");
 printf("Adding command to bash_profile\n");
